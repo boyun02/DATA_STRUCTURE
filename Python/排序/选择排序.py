aae9698750b169e59d2088e98e62a2efd44326c3
boyun02 <1814297932@qq.com>
2023-08-25 23:48:44 +0800

    排序

---
+++ Python/排序/选择排序.py
@@ -0,0 +1,20 @@
+'''
+# 选择排序
+'''
+
+
+
+def selecttionSort(alist):
+    for fillslot in range(len(alist)-1,0,-1):
+        positionOfMax = 0
+        for location in range(1,fillslot+1):
+            if alist[location]>alist[positionOfMax]:
+                positionOfMax = location
+        temp = alist[fillslot]
+        alist[fillslot] = alist[positionOfMax]
+        alist[positionOfMax] = temp
+
+
+alist = [54,26,93,17,77,31,44,55,20]
+selecttionSort(alist)
+print(alist)
