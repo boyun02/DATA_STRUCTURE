3613de2e1f980f0b939379de5d1d0978028ed6f1
boyun02 <1814297932@qq.com>
2023-09-24 13:12:21 +0800

    Create BinaryTree.py

---
+++ Python/树/BinaryTree.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time : 2023/9/23 23:47
+# @Author :wyb
+
+class BinaryTree:
+    """二叉树的链表实现
+    """
+    def __init__(self, root):
+        self.key = root
+        self.leftChild = None
+        self.rightChail = None
+
+    def insertLeft(self, newNode):
+        if self.leftChild == None:
+            self.leftChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.left = self.leftChild
+            self.leftChild = t
+
+    def insertRight(self, newNode):
+        if self.rightChild == None:
+            self.rightChild = BinaryTree(newNode)
+        else:
+            t = BinaryTree(newNode)
+            t.right = self.rightChild
+            self.rightChild = t
+    def isLeaf(self):
+        return ((not self.leftChild) and (not self.rightChild))
+
+    def getRightChild(self):
+        return self.rightChild
+
+    def getLeftChild(self):
+        return self.leftChild
+
+    def setRootVal(self, obj):
+        self.key = obj
+
+    def getRootVal(self, ):
+        return self.key
+
+    def inorder(self):
+        if self.leftChild:
+            self.leftChild.inorder()
+        print(self.key)
+        if self.rightChild:
+            self.rightChild.inorder()
+
+    def postorder(self):
+        if self.leftchile:
+            self.leftChild.postorder()
+        if self.rightChild:
+            self.rightChild.postorder()
+        print(self.key)
+
+    def preorder(self):
+        print(self.key)
+        if self.leftChild:
+            self.leftChild.preorder()
+        if self.rightChild:
+            self.rightChild.preorder()
+
+
+
+
+
+
